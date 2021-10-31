12b03467073e7dfe4391e24df218d5766ed0ccfb
sakil72642 <mission.scic.04@gmail.com>
2021-10-31 21:12:05 +0600

    final

--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import img13 from '../../../images/13.jpg'
+// import img13 from '../../../images/13.jpg'
 import './Service.css'
 
 const Service = (props) => {
